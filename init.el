5bccdda43dfe54481529384e1e3dc5a8248b7bd9
Manh-Ha Vu <manhha@hotmail.fr>
2012-08-10 04:44:34 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,30 @@
+(add-to-list 'load-path "C:/emacs/.emacs.d/")
+(require 'clojure-mode) 
+
+(require 'package)
+;; Add the original Emacs Lisp Package Archive
+(add-to-list 'package-archives
+             '("elpa" . "http://tromey.com/elpa/"))
+;; Add the user-contributed repository
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; (require 'auto-complete)
+;; (require 'ac-slime)
+;; (add-hook 'slime-mode-hook 'set-up-slime-ac)
+;; (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+;; (eval-after-load "auto-complete"
+;;   '(add-to-list 'ac-modes 'slime-repl-mode))
+   
+;; auto complete
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+(setq ac-delay 0.5) ;; eclipse uses 500ms
+ 
+;; configure auto complete to work in slime
+(add-to-list 'load-path "~/.emacs.d/ac-slime")
+(require 'ac-slime)
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
